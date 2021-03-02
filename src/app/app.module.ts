92e0ef65d60d47653923e9e83ddfba2d9b61610b
=FrancineU <uwizeyimanafrancine62@gmail.com>
2021-03-02 15:08:38 +0200

    added my project

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ServiceService} from './services/service.service';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+import { FormComponent } from './form/form.component';
+import { UserInfoComponent } from './user.info/user.info.component';
+import { RouterModule } from '@angular/router';
+import { RoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { UpperCasePipe } from './uppercase.pipe';
+import { HighlightDirective } from './highlight.directive';
+
+
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    RepositoriesComponent,
+    FormComponent,
+    UserInfoComponent,
+    UpperCasePipe,
+    HighlightDirective,
+ 
+  
+    
+  ],
+  imports: [
+    BrowserModule,
+    RoutingModule,
+    RouterModule,
+    FormsModule,
+    HttpClientModule,
+  
+
+  ],
+  providers: [ServiceService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  miApi:  "72f742813269acfa6b9216e17751210c64516871"
+};
